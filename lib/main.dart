2f070677a15d8036cbdb923abf4c1832fcee4d13
Przemysław <313145464+PEnzym@users.noreply.github.com>
2026-09-16 15:16:13 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl_standalone.dart';
 import 'package:provider/provider.dart';
 
@@ -58,7 +57,7 @@ final appSupportedLocales = [
   {'name': 'Italiano', 'locale': Locale('it')},
   {'name': '日本語', 'locale': Locale('ja')},
   {'name': '한국어', 'locale': Locale('ko')},
-  {'name': 'Polski', 'locale': Locale('pl')}
+  {'name': 'Polski', 'locale': Locale('pl')},
   {'name': 'Português', 'locale': Locale('pt')},
   {'name': 'Русский', 'locale': Locale('ru')},
   {
@@ -268,9 +267,7 @@ class CarVitaApp extends StatelessWidget {
             // i18n
             localizationsDelegates: [
               AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
+              ...GlobalMaterialLocalizations.delegates,
             ],
             supportedLocales: appSupportedLocales.map(
               (lang) => lang['locale'] as Locale,
